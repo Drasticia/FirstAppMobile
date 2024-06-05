a73c93b5ae7ec0cd17b0791f01a8fd6ec7ffb009
zaghinugraha <zaghi.nugraha@gmail.com>
2024-06-05 17:37:47 +0700

    .:: create data firestore for reportscreen.dart .:: fixed build.gradle

--- apptubes/lib/screen/location_picker_screen.dart
+++ apptubes/lib/screen/location_picker_screen.dart
@@ -1,3 +1,186 @@
+// import 'package:flutter/material.dart';
+// import 'package:google_maps_flutter/google_maps_flutter.dart';
+// import 'package:geolocator/geolocator.dart';
+// import 'package:geocoding/geocoding.dart';
+// import 'package:flutter_google_places/flutter_google_places.dart';
+// import 'package:google_maps_webservice/places.dart';
+//
+// class LocationPickerScreen extends StatefulWidget {
+//   @override
+//   _LocationPickerScreenState createState() => _LocationPickerScreenState();
+// }
+//
+// class _LocationPickerScreenState extends State<LocationPickerScreen> {
+//   GoogleMapController? _controller;
+//   LatLng? _pickedLocation;
+//   Position? _currentPosition;
+//   String? _address;
+//   final String _apiKey = 'AIzaSyCzksH2MXN7R0PFZdm65iwGRD_FupWRFzU'; // Ganti dengan API Key Anda
+//   late GoogleMapsPlaces _places;
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     _places = GoogleMapsPlaces(apiKey: _apiKey);
+//     _getCurrentLocation();
+//   }
+//
+//   Future<void> _getCurrentLocation() async {
+//     bool serviceEnabled;
+//     LocationPermission permission;
+//
+//     serviceEnabled = await Geolocator.isLocationServiceEnabled();
+//     if (!serviceEnabled) {
+//       return Future.error('Location services are disabled.');
+//     }
+//
+//     permission = await Geolocator.checkPermission();
+//     if (permission == LocationPermission.denied) {
+//       permission = await Geolocator.requestPermission();
+//       if (permission == LocationPermission.denied) {
+//         return Future.error('Location permissions are denied');
+//       }
+//     }
+//
+//     if (permission == LocationPermission.deniedForever) {
+//       return Future.error('Location permissions are permanently denied, we cannot request permissions.');
+//     }
+//
+//     Position position = await Geolocator.getCurrentPosition(
+//       desiredAccuracy: LocationAccuracy.high,
+//     );
+//     setState(() {
+//       _currentPosition = position;
+//     });
+//     if (_controller != null) {
+//       _controller?.moveCamera(
+//         CameraUpdate.newLatLng(
+//           LatLng(position.latitude, position.longitude),
+//         ),
+//       );
+//     }
+//   }
+//
+//   Future<void> _selectLocation(LatLng location) async {
+//     setState(() {
+//       _pickedLocation = location;
+//     });
+//     await _getAddressFromLatLng(location);
+//   }
+//
+//   Future<void> _getAddressFromLatLng(LatLng location) async {
+//     try {
+//       List<Placemark> placemarks = await placemarkFromCoordinates(location.latitude, location.longitude);
+//       Placemark place = placemarks[0];
+//       setState(() {
+//         _address = "${place.street}, ${place.locality}, ${place.postalCode}, ${place.country}";
+//       });
+//     } catch (e) {
+//       print(e);
+//     }
+//   }
+//
+//   Future<void> _handleSearch() async {
+//     Prediction? p = await PlacesAutocomplete.show(
+//       context: context,
+//       apiKey: _apiKey,
+//       mode: Mode.overlay, // Mode.fullscreen to display search in fullscreen mode
+//       language: "en",
+//       components: [Component(Component.country, "id")], // Batasi pencarian ke negara tertentu
+//     );
+//
+//     if (p != null) {
+//       await _displayPrediction(p);
+//     }
+//   }
+//
+//   Future<void> _displayPrediction(Prediction p) async {
+//     PlacesDetailsResponse detail = await _places.getDetailsByPlaceId(p.placeId!);
+//     final lat = detail.result.geometry!.location.lat;
+//     final lng = detail.result.geometry!.location.lng;
+//     final location = LatLng(lat, lng);
+//
+//     _controller?.animateCamera(CameraUpdate.newLatLng(location));
+//     await _selectLocation(location);
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: const Color.fromRGBO(249, 201, 116, 1),
+//         title: Text(
+//           'Select Location',
+//           style: TextStyle(
+//             fontWeight: FontWeight.bold
+//           ),
+//         ),
+//         actions: [
+//           IconButton(
+//             icon: Icon(Icons.search),
+//             onPressed: _handleSearch,
+//           ),
+//         ],
+//       ),
+//       body: Stack(
+//         children: [
+//           if (_currentPosition != null)
+//             GoogleMap(
+//               initialCameraPosition: CameraPosition(
+//                 target: LatLng(
+//                   _currentPosition!.latitude,
+//                   _currentPosition!.longitude,
+//                 ),
+//                 zoom: 16,
+//               ),
+//               onMapCreated: (controller) {
+//                 _controller = controller;
+//                 _controller?.moveCamera(
+//                   CameraUpdate.newLatLng(
+//                     LatLng(_currentPosition!.latitude, _currentPosition!.longitude),
+//                   ),
+//                 );
+//               },
+//               onTap: _selectLocation,
+//               markers: _pickedLocation == null
+//                   ? {}
+//                   : {
+//                 Marker(
+//                   markerId: MarkerId('selected-location'),
+//                   position: _pickedLocation!,
+//                 ),
+//               },
+//             )
+//           else
+//             Center(child: CircularProgressIndicator()),
+//           Positioned(
+//             bottom: 16,
+//             left: 16,
+//             right: 16,
+//             child: ElevatedButton(
+//               onPressed: _pickedLocation == null
+//                   ? null
+//                   : () {
+//                 Navigator.pop(context, _address);
+//               },
+//               style: ElevatedButton.styleFrom(
+//                 backgroundColor: const Color.fromRGBO(249, 201, 116, 1),
+//                     padding: EdgeInsets.symmetric(horizontal: 100, vertical: 12),
+//                     foregroundColor: Colors.black,
+//                     textStyle: TextStyle(
+//                       fontSize: 15,
+//                       fontWeight: FontWeight.bold,
+//                     ),
+//               ),
+//               child: Text('Confirm Location'),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
@@ -15,7 +198,7 @@ class _LocationPickerScreenState extends State<LocationPickerScreen> {
   LatLng? _pickedLocation;
   Position? _currentPosition;
   String? _address;
-  final String _apiKey = 'AIzaSyCzksH2MXN7R0PFZdm65iwGRD_FupWRFzU'; // Ganti dengan API Key Anda
+  final String _apiKey = 'YOUR_API_KEY_HERE';
   late GoogleMapsPlaces _places;
 
   @override
@@ -84,9 +267,9 @@ class _LocationPickerScreenState extends State<LocationPickerScreen> {
     Prediction? p = await PlacesAutocomplete.show(
       context: context,
       apiKey: _apiKey,
-      mode: Mode.overlay, // Mode.fullscreen to display search in fullscreen mode
+      mode: Mode.overlay,
       language: "en",
-      components: [Component(Component.country, "id")], // Batasi pencarian ke negara tertentu
+      components: [Component(Component.country, "id")],
     );
 
     if (p != null) {
@@ -111,9 +294,7 @@ class _LocationPickerScreenState extends State<LocationPickerScreen> {
         backgroundColor: const Color.fromRGBO(249, 201, 116, 1),
         title: Text(
           'Select Location',
-          style: TextStyle(
-            fontWeight: FontWeight.bold
-          ),
+          style: TextStyle(fontWeight: FontWeight.bold),
         ),
         actions: [
           IconButton(
@@ -161,16 +342,20 @@ class _LocationPickerScreenState extends State<LocationPickerScreen> {
               onPressed: _pickedLocation == null
                   ? null
                   : () {
-                Navigator.pop(context, _address);
+                Navigator.pop(context, {
+                  'address': _address,
+                  'lat': _pickedLocation!.latitude,
+                  'lng': _pickedLocation!.longitude,
+                });
               },
               style: ElevatedButton.styleFrom(
                 backgroundColor: const Color.fromRGBO(249, 201, 116, 1),
-                    padding: EdgeInsets.symmetric(horizontal: 100, vertical: 12),
-                    foregroundColor: Colors.black,
-                    textStyle: TextStyle(
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold,
-                    ),
+                padding: EdgeInsets.symmetric(horizontal: 100, vertical: 12),
+                foregroundColor: Colors.black,
+                textStyle: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
               child: Text('Confirm Location'),
             ),
--- apptubes/lib/screen/reportscreen.dart
+++ apptubes/lib/screen/reportscreen.dart
@@ -1,7 +1,257 @@
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+// import 'package:google_maps_flutter/google_maps_flutter.dart';
+// import 'location_picker_screen.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+//
+// class ReportScreen extends StatefulWidget {
+//   const ReportScreen({super.key});
+//
+//   @override
+//   State<ReportScreen> createState() => _ReportScreenState();
+// }
+//
+// class _ReportScreenState extends State<ReportScreen> {
+//
+//   // Variable Controllers
+//   final TextEditingController _nameController = TextEditingController();
+//   final TextEditingController _phoneNumberController = TextEditingController();
+//   final TextEditingController _geolocationController = TextEditingController();
+//   final TextEditingController _detailedInformationController = TextEditingController();
+//
+//   bool isForMyself = true;
+//   String selectedEmergency = '';
+//   String selectedLocationAddress = '[Selected Location]';
+//   List<String> emergencies = [
+//     'Fire',
+//     'Flood',
+//     'Earthquake',
+//     'Tsunami',
+//     'Volcano',
+//     'Landslide',
+//     'Typhoon'
+//   ];
+//
+//   @override
+//
+//   void dispose() {
+//     _nameController.dispose();
+//     _phoneNumberController.dispose();
+//     _geolocationController.dispose();
+//     _detailedInformationController.dispose();
+//     super.dispose();
+//   }
+//
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       resizeToAvoidBottomInset: false,
+//       backgroundColor: Colors.transparent,
+//       appBar: AppBar(
+//         backgroundColor: Colors.transparent,
+//         title: Text(
+//           'Report',
+//           style: TextStyle(
+//             fontWeight: FontWeight.bold,
+//             fontSize: 25,
+//           ),
+//         ),
+//       ),
+//       body: SingleChildScrollView(
+//         child: Padding(
+//           padding: const EdgeInsets.all(16.0),
+//           child: Container(
+//             decoration: BoxDecoration(
+//               color: Colors.white,
+//               border: Border.all(color: Colors.grey),
+//               borderRadius: BorderRadius.circular(15),
+//             ),
+//             padding: EdgeInsets.all(16.0),
+//             child: Column(
+//               children: [
+//                 ToggleButtons(
+//                   borderRadius: BorderRadius.circular(10),
+//                   children: [
+//                     Padding(
+//                       padding: const EdgeInsets.symmetric(horizontal: 46),
+//                       child: Text(
+//                         'For Myself',
+//                         style: TextStyle(
+//                           color: Colors.black,
+//                           fontWeight: FontWeight.bold,
+//                         ),
+//                       ),
+//                     ),
+//                     Padding(
+//                       padding: const EdgeInsets.symmetric(horizontal: 17),
+//                       child: Text(
+//                         'For Someone Else',
+//                         style: TextStyle(
+//                           color: Colors.black,
+//                           fontWeight: FontWeight.bold,
+//                         ),
+//                       ),
+//                     ),
+//                   ],
+//                   isSelected: [isForMyself, !isForMyself],
+//                   onPressed: (int index) {
+//                     setState(() {
+//                       isForMyself = index == 0;
+//                     });
+//                   },
+//                   renderBorder: true,
+//                   selectedBorderColor: Colors.grey.shade400,
+//                   fillColor: const Color.fromRGBO(249, 201, 116, 1),
+//                   borderColor: Colors.grey.shade400,
+//                 ),
+//
+//                 SizedBox(height: 20),
+//                 TextField(
+//                   decoration: InputDecoration(
+//                     labelText: 'Name',
+//                     border: OutlineInputBorder(),
+//                     contentPadding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+//                   ),
+//                   controller: _nameController,
+//                 ),
+//                 SizedBox(height: 10),
+//                 TextField(
+//                   decoration: InputDecoration(
+//                     labelText: 'Phone Number',
+//                     border: OutlineInputBorder(),
+//                     contentPadding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+//                   ),
+//                   controller: _phoneNumberController,
+//                 ),
+//                 SizedBox(height: 10),
+//                 ElevatedButton(
+//                   onPressed: () async {
+//                     var result = await Navigator.push(
+//                       context,
+//                       MaterialPageRoute(
+//                         builder: (context) => LocationPickerScreen(),
+//                       ),
+//                     );
+//                     // result = _geolocationController;
+//                     if (result != null) {
+//                       setState(() {
+//                         selectedLocationAddress = result;
+//                       });
+//                     }
+//                     result = _geolocationController;
+//                   },
+//                   style: ElevatedButton.styleFrom(
+//                     backgroundColor: const Color.fromRGBO(249, 201, 116, 1),
+//                     padding: EdgeInsets.symmetric(horizontal: 100, vertical: 12),
+//                     foregroundColor: Colors.black,
+//                     textStyle: TextStyle(
+//                       fontSize: 15,
+//                       fontWeight: FontWeight.bold,
+//                     ),
+//                   ),
+//                   child: Row(
+//                     mainAxisSize: MainAxisSize.min,
+//                     children: [
+//                       Icon(
+//                         Icons.location_on,
+//                         size: 20,
+//                         color: Colors.black,
+//                       ),
+//                       SizedBox(width: 4),
+//                       Text('Select Location'),
+//                     ],
+//                   ),
+//                 ),
+//                 SizedBox(height: 10),
+//                 Text('Location: $selectedLocationAddress'),
+//                 SizedBox(height: 10),
+//                 TextField(
+//                   decoration: InputDecoration(
+//                     labelText: 'Detail Location',
+//                     border: OutlineInputBorder(),
+//                     contentPadding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+//                   ),
+//                   controller: _detailedInformationController,
+//                 ),
+//                 SizedBox(height: 15),
+//                 Text(
+//                   'Select Emergency',
+//                   style: TextStyle(
+//                     fontWeight: FontWeight.bold,
+//                     fontSize: 18,
+//                   ),
+//                 ),
+//                 SizedBox(height: 10),
+//                 Wrap(
+//                   spacing: 10,
+//                   children: emergencies.map((emergency) {
+//                     return ChoiceChip(
+//                       label: Text(emergency),
+//                       selected: selectedEmergency == emergency,
+//                       selectedColor: const Color.fromRGBO(249, 201, 116, 1),
+//                       onSelected: (bool selected) {
+//                         setState(() {
+//                           selectedEmergency = selected ? emergency : '';
+//                         });
+//                       },
+//                     );
+//                   }).toList(),
+//                 ),
+//                 SizedBox(height: 20),
+//                 ElevatedButton(
+//                   onPressed: _addReportDetails,
+//                   child: Text(
+//                     'Send Report',
+//                     style: TextStyle(
+//                       color: Colors.white,
+//                       fontSize: 17,
+//                     ),
+//                   ),
+//                   style: ElevatedButton.styleFrom(
+//                     elevation: 2,
+//                     padding: EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+//                     backgroundColor: const Color.fromRGBO(190, 48, 0, 1),
+//                     shape: RoundedRectangleBorder(
+//                       borderRadius: BorderRadius.circular(10),
+//                     ),
+//                   ),
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+//
+//   // Create data on Firestore
+//   Future _addReportDetails() async {
+//
+//     String name = _nameController.text;
+//     String phoneNumber = _phoneNumberController.text;
+//     String geolocation = _geolocationController.text;
+//     String detailedInformation = _detailedInformationController.text;
+//     String emergency = selectedEmergency;
+//
+//     if (name != "" && phoneNumber != "" && geolocation != null && emergency != "") {
+//       await FirebaseFirestore.instance.collection('Report').add({
+//         'name': name,
+//         'phonenumber': phoneNumber,
+//         'geolocation': geolocation,
+//         'detailedInformation': detailedInformation,
+//         'emergency': emergency,
+//       });
+//     } else {
+//       print("Create gagal");
+//     }
+//   }
+// }
+
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'location_picker_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:math';
 
 class ReportScreen extends StatefulWidget {
   const ReportScreen({super.key});
@@ -11,16 +261,15 @@ class ReportScreen extends StatefulWidget {
 }
 
 class _ReportScreenState extends State<ReportScreen> {
-
   // Variable Controllers
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _phoneNumberController = TextEditingController();
-  final TextEditingController _geolocationController = TextEditingController();
   final TextEditingController _detailedInformationController = TextEditingController();
 
   bool isForMyself = true;
   String selectedEmergency = '';
   String selectedLocationAddress = '[Selected Location]';
+  LatLng? selectedLocation;
   List<String> emergencies = [
     'Fire',
     'Flood',
@@ -30,17 +279,15 @@ class _ReportScreenState extends State<ReportScreen> {
     'Landslide',
     'Typhoon'
   ];
-  
-  @override
 
+  @override
   void dispose() {
     _nameController.dispose();
     _phoneNumberController.dispose();
-    _geolocationController.dispose();
     _detailedInformationController.dispose();
     super.dispose();
   }
-  
+
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
@@ -102,7 +349,6 @@ class _ReportScreenState extends State<ReportScreen> {
                   fillColor: const Color.fromRGBO(249, 201, 116, 1),
                   borderColor: Colors.grey.shade400,
                 ),
-
                 SizedBox(height: 20),
                 TextField(
                   decoration: InputDecoration(
@@ -132,9 +378,9 @@ class _ReportScreenState extends State<ReportScreen> {
                     );
                     if (result != null) {
                       setState(() {
-                        selectedLocationAddress = result;
+                        selectedLocationAddress = result['address'];
+                        selectedLocation = LatLng(result['lat'], result['lng']);
                       });
-                      result = _geolocationController;
                     }
                   },
                   style: ElevatedButton.styleFrom(
@@ -196,9 +442,7 @@ class _ReportScreenState extends State<ReportScreen> {
                 ),
                 SizedBox(height: 20),
                 ElevatedButton(
-                  onPressed: () {},
-                    
-                  
+                  onPressed: _addReportDetails,
                   child: Text(
                     'Send Report',
                     style: TextStyle(
@@ -224,20 +468,26 @@ class _ReportScreenState extends State<ReportScreen> {
   }
 
   // Create data on Firestore
-  Future _addReportDetails(String name, String phoneNumber, String geolocation, String detailedInformation, String? emergency) async {
+  Future _addReportDetails() async {
+    String name = _nameController.text;
+    String phoneNumber = _phoneNumberController.text;
+    GeoPoint? geolocation = selectedLocation != null ? GeoPoint(selectedLocation!.latitude, selectedLocation!.longitude) : null;
+    String detailedInformation = _detailedInformationController.text;
+    String emergency = selectedEmergency;
+    var i = 1;
 
-    name = _nameController.text;
-    phoneNumber = _phoneNumberController.text;
-    geolocation = _geolocationController.text;
-    detailedInformation = _detailedInformationController.text;
-    emergency = selectedEmergency;
-
-    await FirebaseFirestore.instance.collection('Report').add({
-      'name': name,
-      'phonenumber': phoneNumber,
-      'geolocation': geolocation,
-      'detailedInformation': detailedInformation,
-      'emergency': emergency,
-    });
+    if (name.isNotEmpty && phoneNumber.isNotEmpty && emergency.isNotEmpty) {
+      await FirebaseFirestore.instance
+          .collection('Report')
+          .add({
+        'name': name,
+        'phonenumber': phoneNumber,
+        'geolocation': geolocation,
+        'detailedInformation': detailedInformation,
+        'emergency': emergency,
+      });
+    } else {
+      print("Create gagal");
+    }
   }
 }
