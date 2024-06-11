e42f0a6af35b28a6db31d0d8214e8fb5738140ac
Drasticia <132827705+Drasticia@users.noreply.github.com>
2024-06-11 19:44:48 +0700

    homescreen

--- apptubes/lib/screen/homescreen.dart
+++ apptubes/lib/screen/homescreen.dart
@@ -2,34 +2,34 @@ import 'package:apptubes/models/weatermodel.dart';
 import 'package:apptubes/services/weaterservice.dart';
 import 'package:flutter/material.dart';
 
-import 'EmergencyContactPage.dart';
-import 'RecentInformationPage.dart';
-
 class HomeScreen extends StatefulWidget {
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final _weatherService = weatherService('abe1b99a3ec5c3cb0c5d3ef43264d1ff');
+  final _weatherService =weatherService('abe1b99a3ec5c3cb0c5d3ef43264d1ff');
   Weather? _weather;
 
   _fetchWeather() async {
     String cityname = await _weatherService.getCurrentlyCity();
 
-    try {
+    try{
       final weather = await _weatherService.getWeather(cityname);
       setState(() {
         _weather = weather;
       });
-    } catch (e) {
+    }
+
+    catch (e){
       print(e);
     }
   }
 
   @override
-  void initState() {
+  void initState(){
     super.initState();
+
     _fetchWeather();
   }
 
@@ -43,10 +43,11 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Text(
           'Fire & Disaster',
           style: TextStyle(
-            fontWeight: FontWeight.w700,
+            fontWeight: FontWeight.bold,
             fontSize: 25,
           ),
         ),
+        
       ),
       body: Column(
         children: [
@@ -69,7 +70,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: [
-                    Text('${_weather?.temperature.round()}°C'),
+                    Text('${_weather?.temperature.round()}`C'),
                     SizedBox(width: 8.0),
                     Icon(
                       Icons.wb_sunny_outlined,
@@ -80,122 +81,110 @@ class _HomeScreenState extends State<HomeScreen> {
               ],
             ),
           ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  padding: EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Recent Information',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 8.0),
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey),
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        padding: EdgeInsets.all(16.0),
+                        child: Row(
+                          children: [
+                            SizedBox(width: 16.0),
+                            Center(
+                              child: Text(
+                                'Harusnya disini live map',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 12,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 8.0),
+                      Row(
+                        children: [
+                          Icon(Icons.location_on, color: Colors.blue),
+                          SizedBox(width: 8.0),
+                          Text('Manacik, Karature'),
+                        ],
+                      ),
+                      SizedBox(height: 8.0),
+                      Row(
+                        children: [
+                          Icon(Icons.crisis_alert, color: Colors.red),
+                          SizedBox(width: 8.0),
+                          Text('Disaster Type: Earthquake'),
+                        ],
+                      ),
+                      SizedBox(height: 8.0),
+                      Row(
+                        children: [
+                          Icon(Icons.warning, color: Colors.orange),
+                          SizedBox(width: 8.0),
+                          Text('Criticality: Mild'),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
           Expanded(
-            child: SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 16),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => RecentInformationPage()),
-                      );
-                    },
-                    child: Container(
-                      margin: EdgeInsets.all(10.0),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        border: Border.all(color: Colors.grey),
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      padding: EdgeInsets.all(16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Recent Information',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          SizedBox(height: 8.0),
-                          Container(
-                            decoration: BoxDecoration(
-                              border: Border.all(color: Colors.grey),
-                              borderRadius: BorderRadius.circular(15),
-                            ),
-                            padding: EdgeInsets.all(16.0),
-                            child: Row(
-                              children: [
-                                SizedBox(width: 16.0),
-                                Center(
-                                  child: Text(
-                                    'Harusnya disini live map',
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 12,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          SizedBox(height: 8.0),
-                          Row(
-                            children: [
-                              Icon(Icons.location_on, color: Colors.blue),
-                              SizedBox(width: 8.0),
-                              Text('Manacik, Karature'),
-                            ],
-                          ),
-                          SizedBox(height: 8.0),
-                          Row(
-                            children: [
-                              Icon(Icons.crisis_alert, color: Colors.red),
-                              SizedBox(width: 8.0),
-                              Text('Disaster Type: Earthquake'),
-                            ],
-                          ),
-                          SizedBox(height: 8.0),
-                          Row(
-                            children: [
-                              Icon(Icons.warning, color: Colors.orange),
-                              SizedBox(width: 8.0),
-                              Text('Criticality: Mild'),
-                            ],
-                          ),
-                        ],
+                  Center(
+                    child: Text(
+                      'Emergency Contact',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
                       ),
                     ),
                   ),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => EmergencyContactPage()),
-                      );
-                    },
-                    child: Container(
-                      margin: EdgeInsets.all(10.0),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        border: Border.all(color: Colors.grey),
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      padding: EdgeInsets.all(16.0),
+                  SizedBox(height: 16.0),
+                  Expanded(
+                    child: SingleChildScrollView(
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Center(
-                            child: Text(
-                              'Emergency Contact',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 18,
-                              ),
-                            ),
-                          ),
-                          SizedBox(height: 16.0),
                           Container(
                             decoration: BoxDecoration(
-                              color: Color.fromARGB(255, 182, 182, 182),
+                              color: Colors.white,
                               border: Border.all(color: Colors.grey),
                               borderRadius: BorderRadius.circular(15),
                             ),
                             padding: EdgeInsets.all(16.0),
                             child: Column(
-                              children: [
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children  : [
                                 Text(
                                   'General',
                                   style: TextStyle(
@@ -212,30 +201,36 @@ class _HomeScreenState extends State<HomeScreen> {
                                   title: Text('Fire Station'),
                                   trailing: Icon(Icons.call),
                                 ),
+                                SizedBox(height: 16.0),
+                                Text(
+                                  'Family & Friends',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                ListTile(
+                                  leading: CircleAvatar(
+                                    child: Text('SA'),
+                                  ),
+                                  title: Text('Suleman Abu'),
+                                  trailing: Icon(Icons.call),
+                                ),
+                                ListTile(
+                                  leading: CircleAvatar(
+                                    child: Text('BN'),
+                                  ),
+                                  title: Text('Benjamin Netanyahu'),
+                                  trailing: Icon(Icons.call),
+                                ),
+                                ListTile(
+                                  leading: CircleAvatar(
+                                    child: Text('BN'),
+                                  ),
+                                  title: Text('Benjamin Netanyahu'),
+                                  trailing: Icon(Icons.call),
+                                ),
                               ],
                             ),
-                          ),
-                          SizedBox(height: 20),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                'Keluarga',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              ListTile(
-                                leading: Icon(Icons.local_police),
-                                title: Text('Police Station'),
-                                trailing: Icon(Icons.call),
-                              ),
-                              ListTile(
-                                leading: Icon(Icons.local_fire_department),
-                                title: Text('Fire Station'),
-                                trailing: Icon(Icons.call),
-                              ),
-                            ],
                           ),
                         ],
                       ),
