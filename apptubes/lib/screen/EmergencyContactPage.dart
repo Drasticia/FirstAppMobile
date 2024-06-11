26b133f1bd837acfccd989662a34b52c5683bff9
nwabel <nabilsywl@gmail.com>
2024-06-11 20:03:27 +0700

    .: emergency contact page

--- apptubes/lib/screen/EmergencyContactPage.dart
+++ apptubes/lib/screen/EmergencyContactPage.dart
@@ -10,10 +10,6 @@ class EmergencyContactPage extends StatefulWidget {
 
 class _EmergencyContactPageState extends State<EmergencyContactPage> {
   final List<Map<String, String>> _contacts = [];
-  final List<Map<String, String>> _aparaturNegara = [
-    {'name': 'Polisi', 'initial': 'PL', 'phoneNumber': '+6281283115727'},
-    {'name': 'Pemadam Kebakaran', 'initial': 'PK', 'phoneNumber': '+6281283115727'},
-  ];
 
   void _addContact(String name, String phoneNumber) {
     setState(() {
@@ -29,81 +25,60 @@ class _EmergencyContactPageState extends State<EmergencyContactPage> {
     showDialog(
       context: context,
       builder: (context) {
-        return Dialog(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10.0),
-          ),
-          child: Container(
-            width: MediaQuery.of(context).size.width * 0.8, // 80% of screen width
-            height: MediaQuery.of(context).size.height * 0.35, // 50% of screen height
-            padding: EdgeInsets.all(16.0),
+        return AlertDialog(
+          title: Text('Tambah Kontak Baru'),
+          content: Form(
+            key: _formKey,
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
-                Text(
-                  'Tambah Kontak Baru',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                CustomTextField(
+                  labelText: 'Nama',
+                  hintText: 'Masukkan nama',
+                  controller: _nameController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Nama tidak boleh kosong';
+                    }
+                    return null;
+                  },
                 ),
-                SizedBox(height: 16.0),
-                Form(
-                  key: _formKey,
-                  child: Column(
-                    children: [
-                      CustomTextField(
-                        labelText: 'Nama',
-                        hintText: 'Masukkan nama',
-                        controller: _nameController,
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Nama tidak boleh kosong';
-                          }
-                          return null;
-                        },
-                      ),
-                      CustomTextField(
-                        labelText: 'Nomor Telepon',
-                        hintText: 'Masukkan nomor telepon',
-                        controller: _phoneNumberController,
-                        keyboardType: TextInputType.phone,
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Nomor telepon tidak boleh kosong';
-                          } else if (value.length < 8 || value.length > 15) {
-                            return 'Nomor telepon harus antara 8-15 karakter';
-                          }
-                          return null;
-                        },
-                      ),
-                    ],
-                  ),
-                ),
-                Spacer(),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    TextButton(
-                      onPressed: () => Navigator.of(context).pop(),
-                      child: Text('Batal'),
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        if (_formKey.currentState!.validate()) {
-                          _addContact(_nameController.text, _phoneNumberController.text);
-                          Navigator.of(context).pop();
-                        }
-                      },
-                      child: Text('Tambah'),
-                    ),
-                  ],
+                CustomTextField(
+                  labelText: 'Nomor Telepon',
+                  hintText: 'Masukkan nomor telepon',
+                  controller: _phoneNumberController,
+                  keyboardType: TextInputType.phone,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Nomor telepon tidak boleh kosong';
+                    } else if (value.length < 8 || value.length > 15) {
+                      return 'Nomor telepon harus antara 8-15 karakter';
+                    }
+                    return null;
+                  },
                 ),
               ],
             ),
           ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text('Batal'),
+            ),
+            TextButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  _addContact(_nameController.text, _phoneNumberController.text);
+                  Navigator.of(context).pop();
+                }
+              },
+              child: Text('Tambah'),
+            ),
+          ],
         );
       },
     );
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -129,35 +104,41 @@ class _EmergencyContactPageState extends State<EmergencyContactPage> {
                     'Aparatur Negara',
                     style: TextStyle(
                       fontWeight: FontWeight.w600,
-                      fontSize: 20,
+                      fontSize: 17,
                     ),
                   ),
-                  ListView.separated(
-                    shrinkWrap: true,
-                    physics: NeverScrollableScrollPhysics(),
-                    itemCount: _aparaturNegara.length,
-                    itemBuilder: (context, index) {
-                      final contact = _aparaturNegara[index];
-                      return ListTile(
-                        leading: CircleAvatar(
-                          child: Text(contact['initial']!),
-                        ),
-                        title: Text(contact['name']!),
-                        trailing: IconButton(
-                          onPressed: () async {
-                            await FlutterPhoneDirectCaller.callNumber(contact['phoneNumber']!);
-                          },
-                          icon: ImageIcon(
-                            AssetImage("lib/icons/call.png"),
-                            size: 23.0,
-                            color: Colors.blue,
-                          ),
-                        ),
-                      );
-                    },
-                    separatorBuilder: (context, index) => Divider(),
+                  ListTile(
+                    leading: CircleAvatar(
+                      child: Text('PL'),
+                    ),
+                    title: Text('Polisi'),
+                    trailing: IconButton(
+                      onPressed: () async {
+                        await FlutterPhoneDirectCaller.callNumber('+6281283115727');
+                      },
+                      icon: ImageIcon(
+                        AssetImage("lib/icons/call.png"),
+                        size: 23.0,
+                        color: Colors.blue,
+                      ),
+                    ),
                   ),
-                  Divider(), // Divider bawah list aparatur negara
+                  ListTile(
+                    leading: CircleAvatar(
+                      child: Text('PK'),
+                    ),
+                    title: Text('Pemadam Kebakaran'),
+                    trailing: IconButton(
+                      onPressed: () async {
+                        await FlutterPhoneDirectCaller.callNumber('+6281283115727');
+                      },
+                      icon: ImageIcon(
+                        AssetImage("lib/icons/call.png"),
+                        size: 23.0,
+                        color: Colors.blue,
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
@@ -169,7 +150,7 @@ class _EmergencyContactPageState extends State<EmergencyContactPage> {
                   'Keluarga',
                   style: TextStyle(
                     fontWeight: FontWeight.w600,
-                    fontSize: 20,
+                    fontSize: 17,
                   ),
                 ),
                 ListTile(
@@ -188,34 +169,25 @@ class _EmergencyContactPageState extends State<EmergencyContactPage> {
                     ),
                   ),
                 ),
-                Divider(),
-                ListView.separated(
-                  shrinkWrap: true,
-                  physics: NeverScrollableScrollPhysics(),
-                  itemCount: _contacts.length,
-                  itemBuilder: (context, index) {
-                    final contact = _contacts[index];
-                    return ListTile(
-                      leading: CircleAvatar(
-                        child: Text(contact['name']![0].toUpperCase()),
+                ..._contacts.map((contact) {
+                  return ListTile(
+                    leading: CircleAvatar(
+                      child: Text(contact['name']![0].toUpperCase()),
+                    ),
+                    title: Text(contact['name']!),
+                    subtitle: Text(contact['phoneNumber']!),
+                    trailing: IconButton(
+                      onPressed: () async {
+                        await FlutterPhoneDirectCaller.callNumber(contact['phoneNumber']!);
+                      },
+                      icon: ImageIcon(
+                        AssetImage("lib/icons/call.png"),
+                        size: 23.0,
+                        color: Colors.blue,
                       ),
-                      title: Text(contact['name']!),
-                      subtitle: Text(contact['phoneNumber']!),
-                      trailing: IconButton(
-                        onPressed: () async {
-                          await FlutterPhoneDirectCaller.callNumber(contact['phoneNumber']!);
-                        },
-                        icon: ImageIcon(
-                          AssetImage("lib/icons/call.png"),
-                          size: 23.0,
-                          color: Colors.blue,
-                        ),
-                      ),
-                    );
-                  },
-                  separatorBuilder: (context, index) => Divider(),
-                ),
-
+                    ),
+                  );
+                }).toList(),
               ],
             ),
           ],
